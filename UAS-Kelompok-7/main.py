187911d1308fa0aea6fe2b6a71b6582dc59d5b67
nurrida000 <nurrida.azzahra.1018@gmail.com>
2026-06-05 19:19:08 +0700

    dari nuri

--- UAS-Kelompok-7/main.py
+++ UAS-Kelompok-7/main.py
@@ -1,5 +1,5 @@
 from tampilan_registrasi import sign_up, sign_in
-from display_menu import display_menu
+from Tools_display_menu import display_menu
 from pengaturan_profile import your_profile
 from tampilan_level import lihat_xp, lihat_level
 from tampilan_level import tambah_xp
@@ -8,21 +8,33 @@ print("====================================================")
 print("============== Wellcome to Miss Me 📑 ==============")
 print("====================================================")
 
-while True:
-    print("\n1. Sign Up")
-    print("2. Sign In")
-    print("3. Keluar")
+# Komponen aplikasi Miss Me:
+# 1. Tampilan Registrasi (Sign Up dan Sign In)
+# 2. Menu Utama (Target Harian, Lihat XP, Lihat Level, Pengaturan Profile)
+# 3. Target Harian (Menambahkan target harian, melihat target harian, dan mengevaluasi target harian)
+# 4. Lihat XP (Menampilkan XP yang dimiliki pengguna)
+# 5. Lihat Level (Menampilkan level pengguna berdasarkan XP)
+# 6. Pengaturan Profile (Menampilkan dan mengedit informasi profile pengguna)
 
-    pilihan = input("Pilih: ")
+# Import module (depedensi):
+# ada 2 cara:
+# 1. import nama_module
+# 2. from nama_module import nama_fungsi
 
-    if pilihan == "1":
-        sign_up()
-    elif pilihan == "2":
-        username = sign_in()
+# Contoh module: 
 
-        if username:
-            display_menu(username)
-    elif pilihan == "3":
-        break
-    else:
-        print("Pilihan tidak valid ❌")
+# from tools import Tools_display_menu, Tools_tampilan_registrasi, Tools_pengaturan_profile, Tools_tampilan_level, Tools_our_mission, Tools_target_harian
+
+# __name__ nama file phyton
+
+if __name__ == "__main__":
+    username = sign_in()
+    while True:
+        display_menu(username)
+        import Tools_display_menu
+
+        pilihan = input("Pilih dengan ketikan angka: ")
+
+        is_done = Tools_display_menu(pilihan=pilihan, username=username)
+        if is_done:
+            break
--- UAS-Kelompok-7/tampilan_registrasi.py
+++ UAS-Kelompok-7/tampilan_registrasi.py
@@ -1,43 +1,23 @@
+from Tools_display_menu import Tools_display_menu
+from tampilan_registrasi import sign_up, sign_in
+
 users = {}
 
-print("====================================================")
-print("============== Wellcome to Miss Me 📑 ==============")
-print("====================================================")
-def sign_up():
-    print("\n=== Sign Up ===")
+while True:
+    print("\n1. Sign Up")
+    print("2. Sign In")
+    print("3. Keluar")
 
+    pilihan = input("Pilih: ")
 
-    username = input("Username: ")
-    password = input("Password: ")
+    if pilihan == "1":
+        sign_up()
+    elif pilihan == "2":
+        username = sign_in()
 
-
-    if username in users:
-        print("Username sudah ada!")
-        return
-
-
-    users[username] = {
-        "password": password,
-        "bio": "",
-        "tanggal_lahir": ""
-    }
-
-
-    print("Pendaftaran berhasil!")
-
-
-def sign_in():
-    print("\n=== Sign In ===")
-
-
-    username = input("Username: ")
-    password = input("Password: ")
-
-
-    if username in users and users[username]["password"] == password:
-        print("Login berhasil!")
-        return username
-
-
-    print("Username atau password salah!")
-    return None
+        if username:
+            Tools_display_menu(username)
+    elif pilihan == "3":
+        break
+    else:
+        print("Pilihan tidak valid ❌")
