2190f749aac34358e7b13cb40d1dcce0cd8f1c6d
Sarah Nabila <officialsarahnbl@gmail.com>
2026-06-06 06:55:53 +0700

    pembaruan pada main dan import lingkaran

--- UAS-Kelompok-7/main.py
+++ UAS-Kelompok-7/main.py
@@ -1,4 +1,4 @@
-from tampilan_registrasi import sign_up, sign_in, users
+from tampilan_registrasi import sign_up, sign_in, menu_registrasi
 from pengaturan_profile import your_profile
 from tampilan_level import lihat_xp, lihat_level
 from tampilan_level import tambah_xp
@@ -11,28 +11,6 @@ def header():
 
 def main():
     header()
-
-    while True:
-        print("\n1. Sign Up")
-        print("2. Sign In")
-        print("3. Keluar")
-
-        pilihan = input("Pilih: ")
-
-        if pilihan == "1":
-            sign_up()
-        elif pilihan == "2":
-            username = sign_in()
-
-            if username:
-                display_menu(username, users)
-
-        elif pilihan == "3":
-            header()
-            continue
-        else:
-            print("Pilihan tidak valid ❌")
-
 
 if __name__ == "__main__":
     main()
