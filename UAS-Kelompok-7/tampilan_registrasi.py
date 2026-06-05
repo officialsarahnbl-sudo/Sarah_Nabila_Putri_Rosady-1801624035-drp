151800299e001c520ffdd1b116c681699ba3b292
nurrida000 <nurrida.azzahra.1018@gmail.com>
2026-06-05 17:53:42 +0700

    revisi dikit buat regist

--- UAS-Kelompok-7/tampilan_registrasi.py
+++ UAS-Kelompok-7/tampilan_registrasi.py
@@ -1,14 +1,20 @@
 users = {}
 
+print("====================================================")
+print("============== Wellcome to Miss Me 📑 ==============")
+print("====================================================")
 def sign_up():
     print("\n=== Sign Up ===")
+
 
     username = input("Username: ")
     password = input("Password: ")
 
+
     if username in users:
         print("Username sudah ada!")
         return
+
 
     users[username] = {
         "password": password,
@@ -16,18 +22,22 @@ def sign_up():
         "tanggal_lahir": ""
     }
 
+
     print("Pendaftaran berhasil!")
+
 
 def sign_in():
     print("\n=== Sign In ===")
 
+
     username = input("Username: ")
     password = input("Password: ")
+
 
     if username in users and users[username]["password"] == password:
         print("Login berhasil!")
         return username
 
+
     print("Username atau password salah!")
     return None
-   
