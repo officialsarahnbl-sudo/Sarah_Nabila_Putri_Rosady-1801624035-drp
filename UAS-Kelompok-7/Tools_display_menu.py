ff16cb0101f7f6fa5dd5adcd55d13964aeda4486
Sarah Nabila <officialsarahnbl@gmail.com>
2026-06-05 21:34:00 +0700

    new update

--- UAS-Kelompok-7/Tools_display_menu.py
+++ UAS-Kelompok-7/Tools_display_menu.py
@@ -1,20 +1,17 @@
-from time import strftime
-
-from tampilan_registrasi import users
-from tampilan_registrasi import sign_up, sign_in
+from tampilan_registrasi import sign_in, sigm_up
 from pengaturan_profile import your_profile
 from tampilan_level import lihat_xp, lihat_level
-from tampilan_level import tambah_xp
 from datetime import datetime
 from our_mission import mission as our_mission
 import target_harian
 
 hari_ini = datetime.now().strftime("%d %B %Y")
-#Masuk ke bagian display menu yang diawali dengan search
-def display_menu(username):
+
+def display_menu(username, users):
 
     print(f"\nSelamat datang, {username}")
-    print(f"Waktu saat ini: {hari_ini})")
+    print(f"Waktu saat ini: {hari_ini}")
+
     while True:
         print("\n=== Menu ===")
         print("1. Your Profile")
@@ -24,18 +21,27 @@ def display_menu(username):
         print("5. Our Mission")
         print("6. Logout")
 
-        pilihan = input("Pilih dengan ketikan angka: ")
+        pilihan = input("Pilih dengan ketikan angka: ").strip()
+        print("DEBUG =", repr(pilihan))
 
         if pilihan == "1":
             your_profile(username, users)
-        elif pilihan == "2":
-            lihat_level()
-        elif pilihan == "3":
-            lihat_xp()
-        elif pilihan == "4":
-            target_harian.target_harian(username) # yang merah ini karena di file target_harian.py, fungsinya dijalankan langsung dengan "def"
-            print("Logout berhasil!")
-            break
-        else:
-            print("Pilihan tidak valid")
 
+            elif pilihan == "2":
+                lihat_level()
+
+            elif pilihan == "3":
+                lihat_xp()
+
+            elif pilihan == "4":
+                target_harian.target_harian(username)
+
+            elif pilihan == "5":
+                our_mission()
+
+            elif pilihan == "6":
+                print("MASUK KE PILIHAN 6")
+                return
+
+            else:
+                print("Pilihan tidak valid")
--- UAS-Kelompok-7/tampilan_registrasi.py
+++ UAS-Kelompok-7/tampilan_registrasi.py
@@ -1,23 +1,57 @@
-from Tools_display_menu import Tools_display_menu
-from tampilan_registrasi import sign_up, sign_in
+from Tools_display_menu import display_menu
 
 users = {}
 
+def sign_up():
+    username = input("Username: ")
+    password = input("Password: ")
+
+    if username in users:
+        print("Username sudah ada!")
+        return
+
+    users[username] = {
+        "password": password,
+        "bio": "",
+        "tanggal_lahir": ""
+    }
+
+    print("Pendaftaran berhasil!")
+
+def sign_in():
+    print("\n=== Sign In ===")
+
+    username = input("Username: ")
+    password = input("Password: ")
+
+    if username in users and users[username]["password"] == password:
+        print("Login berhasil!")
+        return username
+
+    print("Username atau password salah!")
+    return None
+
+
 while True:
-    print("\n1. Sign Up")
+    print("\n=== MENU AWAL ===")
+    print("1. Sign Up")
     print("2. Sign In")
     print("3. Keluar")
 
-    pilihan = input("Pilih: ")
+    pilihan = input("Pilih: ").strip()
 
     if pilihan == "1":
         sign_up()
+
     elif pilihan == "2":
         username = sign_in()
 
         if username:
-            Tools_display_menu(username)
+            display_menu(username, users)
+
     elif pilihan == "3":
+        print("Program selesai.")
         break
+
     else:
         print("Pilihan tidak valid ❌")
