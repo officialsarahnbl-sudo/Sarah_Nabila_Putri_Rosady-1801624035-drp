7086f27c8e9190d783e9e06f499043de9ad6e510
Sarah Nabila <officialsarahnbl@gmail.com>
2026-06-30 16:02:37 +0700

    nambahin map

--- UAS-Kelompok-7/komputasi.py
+++ UAS-Kelompok-7/komputasi.py
@@ -1,19 +1,33 @@
+from functools import reduce
+
 def total_xp(users):
-    return sum(user.get('xp', 0) for user in users.values())
+    xp_list = list(map(lambda user: user.get("xp", 0), users.values()))
+    return reduce(lambda x, y: x + y, xp_list, 0)
 
 
 def rata_rata_level(users):
     if not users:
         return 0
-    return round(sum(user.get('level', 1) for user in users.values()) / len(users), 2)
+
+    level_list = list(map(lambda user: user.get("level", 1), users.values()))
+    total_level = reduce(lambda x, y: x + y, level_list, 0)
+
+    return round(total_level / len(level_list), 2)
 
 
 def pengguna_tertinggi_xp(users):
     if not users:
         return None, 0
-    username, data = max(users.items(), key=lambda item: item[1].get('xp', 0))
-    return username, data.get('xp', 0)
 
+    # Filter (contoh: hanya pengguna yang punya XP)
+    data = list(filter(lambda item: item[1].get("xp", 0) >= 0, users.items()))
+
+    # Sort berdasarkan XP
+    data = sorted(data, key=lambda item: item[1].get("xp", 0), reverse=True)
+
+    username, info = data[0]
+
+    return username, info.get("xp", 0)
 
 def tampilkan_komputasi(users):
     print('\n=== Komputasi Singkat ===')
